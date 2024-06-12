fd5245c226502d71485cdf025b415fa269b6c996
Alireza <42074597+arbor0711@users.noreply.github.com>
2024-06-12 13:28:08 +0200

    Add responsive sidebar to home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,7 @@
 <script setup>
-import { toRefs } from "vue";
+import { toRefs, computed } from "vue";
 import { useNews, useToggle } from "../stores";
+import { useRouter } from "vue-router";
 import NewsCard from "./NewsCard.vue";
 
 const newsStore = useNews();
@@ -9,11 +10,32 @@ newsStore.fetchAllNews();
 const toggle = useToggle();
 const { showSearch } = toRefs(toggle);
 showSearch.value = false;
+
+// make it reactive by using computed
+const newsTitle = computed(() => {
+  return newsStore.news
+    .slice(0, 7)
+    .map(
+      (news) =>
+        // news.title.length > 15 ? news.title.substring(0, 50) + "..." :
+        news.title
+    )
+    .sort((a, b) => a.localeCompare(b));
+});
+
+const router = useRouter();
+const handleClick = (title) => {
+  // Accept title as a parameter
+  router.push({
+    name: "news-detail",
+    params: { title },
+  });
+};
 </script>
 
 <template>
-  <div>
-    <div class="news-container">
+  <div class="container">
+    <main class="news">
       <NewsCard
         v-for="(
           { title, description, url, urlToImage }, index
@@ -24,12 +46,56 @@ showSearch.value = false;
         :url="url"
         :imageUrl="urlToImage"
       />
-    </div>
+    </main>
+    <aside class="sidebar">
+      <h2>Hot News</h2>
+      <ul>
+        <li
+          @click="handleClick(title)"
+          v-for="(title, index) in newsTitle"
+          :key="index"
+        >
+          {{ title }}
+        </li>
+      </ul>
+    </aside>
   </div>
 </template>
 
 <style>
-.news-container {
+.news {
   margin-top: 2rem;
+}
+.sidebar {
+  display: none;
+}
+
+@media screen and (min-width: 800px) {
+  .news {
+    width: 70%;
+  }
+  .sidebar {
+    display: block;
+    width: 25%;
+    position: fixed;
+    top: 150px;
+    right: 20px;
+    height: 75vh;
+    overflow: hidden;
+    border: 1px solid #ccc;
+    padding: 0 2rem 2rem;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    font-size: 1.5;
+    margin-bottom: 2rem;
+    border-bottom: 1px solid #ccc;
+    cursor: pointer;
+  }
 }
 </style>
